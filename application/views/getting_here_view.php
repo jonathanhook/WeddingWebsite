ea5eb115140a55f4906c93a29a3049cc766ec0f6
Jonathan Hook <jonathan.hook@ncl.ac.uk>
2013-12-23 23:11:49 +0000

    Added getting there page

---
+++ application/views/getting_here_view.php
@@ -0,0 +1,406 @@
+<div class="body_container">
+	<div class="flower_heading">
+		<img src="<?php echo base_url();?>assets/body/title_flourish.png" />
+		Getting to and from the Czech Republic	
+		<img src="<?php echo base_url();?>assets/body/title_flourish.png" />
+	</div>
+
+	<p>
+		Our wedding and reception will be held at <a href='venue'>Zámek Liblice</a>, which is around 40km to the north of Prague in the Czech Republic.
+		The most conveinent way to travel from the UK to our wedding will be to fly to Prague's Václav Havel Airport.
+		We've compiled a list of flights to Prague from a number of main UK airports, which we hope will be helpful when booking your trip.
+	</p>
+
+	<div class="panel panel-default flight_table">
+	  <div class="panel-heading">
+	  	Flights to Prague
+	  </div>
+
+	  <table class="table">
+	    <tr>
+	    	<th>Airport</th>
+	    	<th>Date</th>
+	    	<th>Departs</th>
+	    	<th>Arrives</th>
+	    	<th>Airline</th>
+	    </tr>
+	    <!-- BRISTOL -->
+	    <tr>
+	    	<td>Bristol</td>
+	    	<td>Thur 10th July</td>
+	    	<td>18:20</td>
+	    	<td>21:25</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+
+ 		<!-- EAST MIDLANDS
+	    <tr>
+	    	<td>East Midlands</td>
+	    	<td>Fri 11th July</td>
+	    	<td>07:00</td>
+	    	<td>10:10</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>-->
+
+	    <!-- EDINBURGH -->
+	    <tr>
+	    	<td>Edinburgh</td>
+	    	<td>Thur 10th July</td>
+	    	<td>09:40</td>
+	    	<td>13:05</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>Edinburgh</td>
+	    	<td>Thur 10th July</td>
+	    	<td>10:30</td>
+	    	<td>14:45</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+	    <!--<tr>
+	    	<td>Edinburgh</td>
+	    	<td>Fri 11th July</td>
+	    	<td>07:30</td>
+	    	<td>10:55</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>-->
+
+	    <!-- LEEDS -->
+	   	<tr>
+	    	<td>Leeds</td>
+	    	<td>Thur 10th July</td>
+	    	<td>07:10</td>
+	    	<td>10:15</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+
+	    <!-- LONDON GATWICK -->
+	    <tr>
+	    	<td>London Gatwick</td>
+	    	<td>Wed 9th July</td>
+	    	<td>08:55</td>
+	    	<td>11:50</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Gatwick</td>
+	    	<td>Wed 9th July</td>
+	    	<td>18:25</td>
+	    	<td>21:20</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	   <tr>
+	    	<td>London Gatwick</td>
+	    	<td>Thur 10th July</td>
+	    	<td>08:55</td>
+	    	<td>11:50</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Gatwick</td>
+	    	<td>Thur 10th July</td>
+	    	<td>18:25</td>
+	    	<td>21:20</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+
+		<!-- LONDON LUTON -->
+	    <tr>
+	    	<td>London Luton</td>
+	    	<td>Thur 10th July</td>
+	    	<td>21:20</td>
+	    	<td>00:10</td>
+	    	<td><a href="http://wizzair.com">WizzAir</a></td>
+	    </tr>
+
+		<!-- LONDON STANSTED -->
+	    <tr>
+	    	<td>London Stansted</td>
+	    	<td>Wed 9th July</td>
+	    	<td>13:00</td>
+	    	<td>15:50</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Stansted</td>
+	    	<td>Wed 9th July</td>
+	    	<td>19:20</td>
+	    	<td>22:10</td>
+	    	<td><a href="http://www.ryanair.com">Ryanair</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Stansted</td>
+	    	<td>Thur 10th July</td>
+	    	<td>18:20</td>
+	    	<td>21:10</td>
+	    	<td><a href="http://www.ryanair.com">Ryanair</a></td>
+	    </tr>
+
+	    <!-- MANCHESTER -->
+	    <tr>
+	    	<td>Manchester</td>
+	    	<td>Wed 9th July</td>
+	    	<td>16:00</td>
+	    	<td>19:05</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+	    <tr>
+	    	<td>Manchester</td>
+	    	<td>Wed 9th July</td>
+	    	<td>18:05</td>
+	    	<td>21:10</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>Manchester</td>
+	    	<td>Thur 10th July</td>
+	    	<td>06:50</td>
+	    	<td>09:55</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+	    <!--<tr>
+	    	<td>Manchester</td>
+	    	<td>Fri 11th July</td>
+	    	<td>06:50</td>
+	    	<td>09:55</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>-->
+	  </table>
+	</div>
+
+	<div class="panel panel-default flight_table">
+	  <div class="panel-heading">
+	  	Flights from Prague
+	  </div>
+
+	  <table class="table">
+	    <tr>
+	    	<th>Airport</th>
+	    	<th>Date</th>
+	    	<th>Departs</th>
+	    	<th>Arrives</th>
+	    	<th>Airline</th>
+	    </tr>
+
+	    <!-- BRISTOL -->
+	    <tr>
+	    	<td>Bristol</td>
+	    	<td>Sun 13th July</td>
+	    	<td>21:55</td>
+	    	<td>23:00</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>Bristol</td>
+	    	<td>Mon 14th July</td>
+	    	<td>21:55</td>
+	    	<td>23:00</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+
+	    <!-- EAST MIDLANDS -->
+	    <tr>
+	    	<td>East Midlands</td>
+	    	<td>Mon 14th July</td>
+	    	<td>21:35</td>
+	    	<td>22:50</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+
+	    <!-- EDINBURGH -->
+	    <tr>
+	    	<td>Edinburgh</td>
+	    	<td>Sun 13th July</td>
+	    	<td>11:25</td>
+	    	<td>12:45</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>Edinburgh</td>
+	    	<td>Mon 14th July</td>
+	    	<td>11:25</td>
+	    	<td>12:45</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>Edinburgh</td>
+	    	<td>Mon 14th July</td>
+	    	<td>18:20</td>
+	    	<td>19:45</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+
+	    <!-- LEEDS -->
+	    <tr>
+	    	<td>Leeds</td>
+	    	<td>Sun 13th July</td>
+	    	<td>16:45</td>
+	    	<td>18:00</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+	    <tr>
+	    	<td>Leeds</td>
+	    	<td>Mon 14th July</td>
+	    	<td>21:15</td>
+	    	<td>22:30</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+
+		<!-- LONDON GATWICK -->
+	    <tr>
+	    	<td>London Gatwick</td>
+	    	<td>Sat 12th July</td>
+	    	<td>12:40</td>
+	    	<td>13:45</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Gatwick</td>
+	    	<td>Sun 13th July</td>
+	    	<td>12:55</td>
+	    	<td>14:00</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Gatwick</td>
+	    	<td>Sun 13th July</td>
+	    	<td>21:50</td>
+	    	<td>22:55</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Gatwick</td>
+	    	<td>Mon 14th July</td>
+	    	<td>12:20</td>
+	    	<td>13:20</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Gatwick</td>
+	    	<td>Mon 14th July</td>
+	    	<td>21:50</td>
+	    	<td>22:55</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+
+		<!-- LONDON LUTON -->
+	    <tr>
+	    	<td>London Luton</td>
+	    	<td>Sat 12th July</td>
+	    	<td>14:30</td>
+	    	<td>15:30</td>
+	    	<td><a href="http://wizzair.com">WizzAir</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Luton</td>
+	    	<td>Sun 13th July</td>
+	    	<td>14:30</td>
+	    	<td>15:30</td>
+	    	<td><a href="http://wizzair.com">WizzAir</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Luton</td>
+	    	<td>Sun 13th July</td>
+	    	<td>19:20</td>
+	    	<td>20:20</td>
+	    	<td><a href="http://wizzair.com">WizzAir</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Luton</td>
+	    	<td>Mon 14th July</td>
+	    	<td>14:30</td>
+	    	<td>15:30</td>
+	    	<td><a href="http://wizzair.com">WizzAir</a></td>
+	    </tr>
+
+	    <!-- LONDON STANSTED -->
+	    <tr>
+	    	<td>London Stansted</td>
+	    	<td>Sun 13th July</td>
+	    	<td>21:40</td>
+	    	<td>22:35</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Stansted</td>
+	    	<td>Sun 13th July</td>
+	    	<td>22:35</td>
+	    	<td>23:25</td>
+	    	<td><a href="http://www.ryanair.com">Ryanair</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Stansted</td>
+	    	<td>Mon 14th July</td>
+	    	<td>21:40</td>
+	    	<td>23:25</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>
+	    <tr>
+	    	<td>London Stansted</td>
+	    	<td>Mon 14th July</td>
+	    	<td>22:35</td>
+	    	<td>23:25</td>
+	    	<td><a href="http://www.ryanair.com">Ryanair</a></td>
+	    </tr>
+
+		<!-- MANCHESTER -->
+	    <tr>
+	    	<td>Manchester</td>
+	    	<td>Sun 13th July</td>
+	    	<td>16:20</td>
+	    	<td>17:35</td>
+	    	<td><a href="http://www.easyjet.com">EasyJet</a></td>
+	    </tr>	    
+	    <tr>
+	    	<td>Manchester</td>
+	    	<td>Sun 13th July</td>
+	    	<td>20:50</td>
+	    	<td>22:05</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+	    <tr>
+	    	<td>Manchester</td>
+	    	<td>Mon 14th July</td>
+	    	<td>20:50</td>
+	    	<td>22:05</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+
+	    <!-- NEWCASTLE -->
+	    <tr>
+	    	<td>Newcastle</td>
+	    	<td>Mon 14th July</td>
+	    	<td>18:25</td>
+	    	<td>19:45</td>
+	    	<td><a href="http://www.jet2.com">Jet2</a></td>
+	    </tr>
+	  </table>
+	</div>
+
+	<iframe class="pull-right polaroid" width="480" height="360" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d163435.06905056577!2d14.532901050390624!3d50.20464382703479!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470be42f5b0d5ec7%3A0x283cde8c95333546!2sZamek+Liblice!5e0!3m2!1sen!2s!4v1387835842920"></iframe>
+
+	<div class="flower_heading">
+		<img src="<?php echo base_url();?>assets/body/title_flourish.png" />
+		Getting to the Venue
+		<img src="<?php echo base_url();?>assets/body/title_flourish.png" />
+	</div>
+
+	<p>
+		Our wedding and reception will be held at <a href='venue'>Zámek Liblice</a>. The venue is accessible by car and taxi, and by train from Prague.
+	</p>
+
+	<h2 class="sub_heading">By Car</h2>
+	<p>
+		From Prague city centre, take the E55/8 to Melnik and then turn right onto the R16 (<a href="https://goo.gl/maps/L8e4f">download directions</a>). 
+	</p>
+	<p>
+		From Prague airport, take the R7 until exit 18 and then join the R16 via the R23644 (<a href="https://goo.gl/maps/tbm9x">download directions</a>).
+	</p>
+
+	<h2 class="sub_heading">By Train</h2>
+	<p>
+		There are hourly trains from <a href="https://goo.gl/maps/in2Gg">Prague Main Station</a> (Praha Hlavní Nádraží) to <a href="https://goo.gl/maps/pfH4Z">Byšice</a>, which is 2km from the venue. 
+		Trains leave at 45 minutes past the hour and take an hour. Most trains are direct, but a small number require a change at Všetaty (<a href="http://www.cd.cz/spojeni/conn.aspx?_s_icmp=smvs&f=Prague&date=11.07.2014&t=Bysice&time=15%3a00&v=&option=10&byarr=false&cmd=cmdSearch">view timetable</a>).
+		Hlavní Nádraží is in the centre of Prague and has a metro stop on the red line.
+	</p>
+</div> 
