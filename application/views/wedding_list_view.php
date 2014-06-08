8f6e506ddf3117dc5aa1e80fbaa97a9824ebf136
Jonathan Hook <jonathan.hook@ncl.ac.uk>
2014-06-08 17:02:53 +0100

    Wedding list live

--- application/views/wedding_list_view.php
+++ application/views/wedding_list_view.php
@@ -4,8 +4,19 @@
 		Wedding Gift List
 		<img src="<?php echo base_url();?>assets/body/title_flourish.png" />
 	</div>
-
 	<p>
-		Please check back later for details of our wedding gift list.
+		Should you wish you give us a wedding gift, we have a gift list with John Lewis, or, if you'd prefer to contribute to our honeymoon, a gift list with Kuoni.
+	</p>
+	<p>
+		If you'd like to contribute towards our honeymoon in St Lucia, please visit <a href="http://www.kuoni.co.uk/gift-list">the Kuoni gift list website</a> and enter our gift list number: 29503.
+	</p>
+	<p>
+		Alternatively, if you would like to buy us something for our new home, our John Lewis gift list number is 600003. You can purchase from the list in store, <a href="https://www.johnlewisgiftlist.com">online</a> or by calling 0345 600 2202.
+	</p>
+	<p>
+		Thank you!
+	</p>
+	<p>
+		Lucy & Jon xxx
 	</p>
 </div>
